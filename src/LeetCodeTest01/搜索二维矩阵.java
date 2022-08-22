0ecf1d97a778fba184027ffc512f1b6d547f59cd
jxy <1341472965@qq.com>
2022-08-22 19:11:16 +0800

    第一次提交

--- src/LeetCodeTest01/搜索二维矩阵.java
+++ src/LeetCodeTest01/搜索二维矩阵.java
@@ -1,2 +1,7 @@
-package LeetCodeTest01;public class 搜索二维矩阵 {
+package LeetCodeTest01;
+
+public class 搜索二维矩阵 {
+    public static void main(String[] args) {
+        System.out.println("test--2022年8月22日");
+    }
 }
